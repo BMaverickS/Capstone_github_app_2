875335d23066deb00667e1170445430ecd781a10
BMaverickS <49307098+BMaverickS@users.noreply.github.com>
2021-07-25 19:08:35 +0700

    Update ApiService.kt

--- core/src/main/java/com/capstone_github_app_2/core/data/source/remote/network/ApiService.kt
+++ core/src/main/java/com/capstone_github_app_2/core/data/source/remote/network/ApiService.kt
@@ -9,10 +9,10 @@ import retrofit2.http.Query
 
 interface ApiService {
     @GET("search/users")
-    @Headers("Authorization: token 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")
+    @Headers("Authorization: token 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")
     suspend fun getSearchListData(@Query("q") username : String) : SearchListResponse
 
     @GET("users/{username}")
-    @Headers("Authorization: token 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")
+    @Headers("Authorization: token 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")
     suspend fun getUserData(@Path("username") username : String) : UserResponse
 }
